213af1ca0ce39d9ddcaabe4f09f775f69dadd392
Ziru Zou <jiyucoding@yahoo.com>
2023-01-16 16:19:01 -0500

    grab form element, add event listener on it and remove default behaviour

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const inputValue = event.target["new-task-description"].value;
+  })
+
 });
+
+
