37d1d17159bd88776901bd006d88c0854385bb21
Ziru Zou <jiyucoding@yahoo.com>
2023-01-17 08:53:20 -0500

    add priority dropdown, add edit form and update form when click save

--- src/index.js
+++ src/index.js
@@ -12,12 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
     // select current option
     const select = document.querySelector("select")
     const selectedOption = select.options[select.selectedIndex].value
-    console.log(selectedOption)
 
     buildTodoList(inputValue, username, dateDue, selectedOption);
-    console.log(inputValue)
-    console.log(username)
-    console.log(dateDue)
+    // console.log(inputValue)
+    // console.log(username)
+    // console.log(dateDue)
   })
 
   addInputFields(form);
@@ -27,9 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 function buildTodoList(todo, name, date, option) {
-  let deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "X";
-  deleteBtn.addEventListener("click", (event) => handleDelete(event));
+  const deleteBtn = createDeleteBtn();
 
   let li = document.createElement("li");
   li.innerHTML = `Todo: ${todo}<br>By: ${name}<br>Due by: ${date}<br>`
@@ -37,21 +34,104 @@ function buildTodoList(todo, name, date, option) {
   const color = optionColors(option);
   li.style.backgroundColor = color;
   
-
   const tasks = document.querySelector("#list #tasks")
   tasks.appendChild(li)
 
+  const editBtn = createEditBtn()
+  li.appendChild(editBtn)
 } 
 
+function createEditBtn() {
+    // create edit btn
+    const li = document.querySelector("#list #tasks li")
+    let editBtn = document.createElement("button")
+    editBtn.textContent = "Edit";
+    li.appendChild(editBtn)
+    editBtn.addEventListener("click", (event) => handleEdit(event));
+    return editBtn
+}
+
+function createDeleteBtn() {
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.addEventListener("click", (event) => handleDelete(event));
+  return deleteBtn
+}
+
+// handle buttons
 function handleDelete(e) {
   e.target.parentNode.remove(); 
 }
 
 
-function addInputFields(form) {
-  const taskInput = form.querySelector("#new-task-description");
-  const submitBtn = form.querySelector("input[type=submit]");
+function handleEdit(e) {
+  const li = e.target.parentNode
 
+  const editForm = document.createElement("form")
+  editForm.setAttribute("id", "new-form")
+  editForm.setAttribute("method", "POST")
+
+  let editTodo = document.createElement("label");
+  editTodo.setAttribute("for", "new-todo");
+  editTodo.textContent = "Task Description: ";
+  let editInput = document.createElement("input");
+  editInput.setAttribute("id", "new-todo");
+  editInput.placeholder = "description"
+
+  
+  let editName = document.createElement("label");
+  editName.setAttribute("for", "new-name");
+  editName.textContent = "Username: ";
+  let nameInput = document.createElement("input");
+  nameInput.setAttribute("id", "new-name");
+  nameInput.placeholder = "Username"
+
+  let editDate = document.createElement("label");
+  editDate.setAttribute("for", "new-date");
+  editDate.textContent = "Due Date: ";
+  let newDate = document.createElement("input");
+  newDate.setAttribute("id", "new-date");
+  newDate.textContent = "mm/dd/yyyy: ";
+  
+  let save = document.createElement("input")
+  save.setAttribute("type", "submit")
+  save.setAttribute("value", "SAVE")
+
+  editForm.append(editTodo, editInput, editName, nameInput, editDate, newDate)
+  dropDown(editForm)
+  editForm.append(save)
+
+  li.append(editForm)
+  editForm.addEventListener("submit", (event) => updateTodo(event))
+  
+}
+
+// update todo
+function updateTodo(e) {
+  e.preventDefault()
+  const newTask = e.target["new-todo"].value;
+  const username = document.querySelector("#new-name").value
+  const dateDue = document.querySelector("#new-date").value
+
+
+  // select current option
+  const select = document.querySelector("#new-form select")
+  const selectedOption = select.options[select.selectedIndex].value
+  const newColor = optionColors(selectedOption)
+
+  // get original todo form
+  const li = document.querySelector("#tasks li")
+  li.innerHTML = `Todo: ${newTask}<br>By: ${username}<br>Due by: ${dateDue}<br>`
+  const deleteBtn = createDeleteBtn();
+  const editBtn = createEditBtn()
+  li.appendChild(deleteBtn);
+  li.appendChild(editBtn);
+  li.style.backgroundColor = newColor
+
+  
+}
+
+function addInputFields(form) {
   // create user label and input
   let userLabel = document.createElement("label");
   userLabel.setAttribute("for", "new-task-user");
@@ -110,3 +190,5 @@ function optionColors(option) {
     return "green"
   }
 }
+
+
